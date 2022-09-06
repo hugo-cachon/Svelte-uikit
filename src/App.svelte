9b09dfc03e7b66a243473bc72be130d32421d0a5
Hugo <hugo.cachon@hetic.net>
2022-09-06 11:12:37 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+import Button from "./lib/Button.svelte";
+import Card from "./lib/Card.svelte";
+</script>
+
+<Button buttonType="primary"></Button>
+
+<Card username="Hugo" 
+      imageURL="https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      profileURl="https://randomuser.me/api" />
+      
+
+
+
+
---
+++ src/lib/Card.svelte
@@ -0,0 +1,46 @@
+<script>
+export let profileURl;
+export let imageURL;
+export let username;
+
+</script>
+
+<div class="card-container">
+    <div class="card">
+        <figure>
+            <img src="{imageURL}" alt="random-user">
+            <figcaption>{username}</figcaption>
+        </figure>
+
+    <button>
+        <a href="{profileURl}" target="_blank">Profile</a>
+    </button>
+</div>
+</div>
+
+<style>
+    .card-container {
+        border: 1px solid black;
+        max-width: 20em;
+        background-color: rgba(211, 211, 211, 0.652);
+    }
+    .card {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+    figure {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    img {
+        border-radius: 25%;
+    }
+    button {
+        width: auto;
+        height: 3em;
+        margin: 1em;
+    }
+</style>
