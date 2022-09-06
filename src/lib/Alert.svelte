d817e9b52d3d94ce407d913b4a687636dd203fe7
Noham <72859612+Noh4m@users.noreply.github.com>
2022-09-06 14:00:29 +0200

    new components

---
+++ src/lib/Alert.svelte
@@ -0,0 +1,50 @@
+<script>
+    export let status = 'valid'
+    export let textButton = `Post has been ${status} !`;
+    export let alertType = 'valid'
+      
+    </script>
+    <style>
+     
+    .valid {
+      background-color: #78c67b;
+      border: solid 2px #1e5d20;
+      border-radius: 10px;
+      color: #1e5d20;
+      font-weight: 600;
+      padding: 20px 50px;
+    }
+    .archived {
+      background-color: #c89658;
+      border: solid 2px #86591e;
+      border-radius: 10px;
+      color: #86591e;
+      font-weight: 600;
+      padding: 20px 50px;
+
+    }
+    .deleted {
+      background-color: #c85858;
+      border: solid 2px #861e1e;
+      border-radius: 10px;
+      color: #861e1e;
+      font-weight: 600;
+      padding: 20px 50px;
+
+    }
+    .updated {
+      background-color: #789cc6;
+      border: solid 2px #1e325d;
+      border-radius: 10px;
+      color: #1e325d;
+      font-weight: 600;
+      padding: 20px 50px;
+    }
+    
+    </style>
+    
+    <div class={alertType}> 
+        { textButton } 
+    </div>
+    
+    
---
+++ src/lib/Newsletter.svelte
@@ -0,0 +1,31 @@
+<script>
+    import Button from "./Button.svelte";
+    export let title = ''
+    export let richText = ''
+</script>
+
+<div class="container">
+    <h2> { title } </h2>
+    <p> { richText } </p>
+    <div>
+        <input type="text" placeholder="Email Adress" />
+        <Button  textButton="Sign Up" buttonType="danger"/>
+    </div>
+</div>
+
+
+<style>
+    .container{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    h2{
+        max-width: 400px;
+        margin-bottom: 10px;
+    }
+    p{
+        margin-top: 0;
+        max-width: 500px;
+    }
+</style>
