77df7c03acb55aab567c330857cd51d9940eea88
Noham <72859612+Noh4m@users.noreply.github.com>
2022-09-06 14:00:41 +0200

    import nex components

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,10 @@ import Card from "./lib/Card.svelte";
 import Input from "./lib/Input.svelte";
 import Menu from "./lib/Menu.svelte";
 const menuItems = ["Item1", "Item2", "Item3"];
+import Button from './lib/Button.svelte';
+import Alert from './lib/Alert.svelte';
+import Newsletter from './lib/Newsletter.svelte';
+     
 </script>
 
 <Card username="John" 
@@ -10,11 +14,18 @@ const menuItems = ["Item1", "Item2", "Item3"];
       profileURl="https://randomuser.me/api" />
 
 <Input inputType="primary" placeholder="Primary"/>
-
 <Menu {menuItems} />
 
-
-
+<Button buttonType="danger" textButton="click"/>
+<Alert />
+<Alert alertType="archived" status="archived"/>
+<Alert alertType="deleted" status="deleted"/>
+<Alert alertType="updated" status="upadate"/>
+<Newsletter  
+title="Lorem, ipsum dolor sit amet consectetur adipisicing elit" 
+richText="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae dolor officia blanditiis repellat in, 
+vero, aperiam porro ipsum laboriosam consequuntur exercitationem incidunt tempora nisi?"/>
+      
       
 
 
