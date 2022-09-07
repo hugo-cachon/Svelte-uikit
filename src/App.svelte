4685715ab35e46302d6cc3c44364f678a0e2ce09
Hugo <hugo.cachon@hetic.net>
2022-09-07 11:13:03 +0200

    Details

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,9 @@
 import Card from "./lib/Card.svelte";
 import Input from "./lib/Input.svelte";
 import Menu from "./lib/Menu.svelte";
+import Details from "./lib/Details.svelte";
 const menuItems = ["Item1", "Item2", "Item3"];
+let src = "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1d/b9/99/5c/moraine-lake-photo-taken.jpg?w=1000&h=-1&s=1";
 </script>
 
 <Card username="John" 
@@ -13,6 +15,7 @@ const menuItems = ["Item1", "Item2", "Item3"];
 
 <Menu {menuItems} />
 
+<Details {src} alt="Moraine Lake" detailsTitle="DetailsTitle" detailsText="DetailsText"/>
 
 
       
